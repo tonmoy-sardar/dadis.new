ed1d859886460bcc19e1036d36d06ecc4b328505
Kalyan Acharya <tonmoy.1984@gmail.com>
2020-02-17 12:06:30 +0530

    change consumer key and secret key

--- src/core/global.ts
+++ src/core/global.ts
@@ -1,12 +1,23 @@
-export const apiEndpoint: string = 'http://dadis.in/wp-json/wc/v3/';
-export const consumer_key:  string = "ck_2e7ec8b0085e658f202e7e56e6c82dcdd8dca9d0";
-export const consumer_secret:  string = "cs_2352d54276862b9fcc4aa2b7840d6b19e37bb3bd";
+// live server
+// export const apiEndpoint: string = 'http://dadis.in/wp-json/wc/v3/';
+// export const consumer_key:  string = "ck_2e7ec8b0085e658f202e7e56e6c82dcdd8dca9d0";
+// export const consumer_secret:  string = "cs_2352d54276862b9fcc4aa2b7840d6b19e37bb3bd";
+
+// local  
+
 // export const apiEndpoint: string = 'http://166.62.54.122/dadi/wp-json/wc/v3/';
 // export const consumer_key:  string = "ck_c4b041bca0fb7c2098f36bf1fab6df8467509091";
 // export const consumer_secret:  string = "cs_10788e156bb5892fc3cbc07bfeabd257279e4488";
+
+//dadis live New
+
+export const apiEndpoint: string = 'http://dadis.in/wp-json/wc/v3/';
+export const consumer_key:  string = "ck_21698385283489e2b664d80c3e04b4d3b762c68f";
+export const consumer_secret:  string = "cs_0da03e5bfa80ca0ba4c8c867077871fdff358c86";
 
 export const img_base_url: string = '';
 export const itemPerPage: number = 10;
 export const paginationMaxSize: number = 2;
 export const server_key: string = '';
 export const firebase_url: string = '';
+
